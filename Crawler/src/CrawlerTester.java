12cc210651a5ba194f8fc894ce3ec1583a5a47c8
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2023-04-16 20:05:13 +0200

    gignore

---
+++ Crawler/src/CrawlerTester.java
@@ -0,0 +1,22 @@
+public class CrawlerTester {
+    public static void main(String[] args) {
+
+        CrawlerIMP CR=new CrawlerIMP("./Seed/Seed.txt");
+        Thread[] threads=new Thread[5];
+        for(int i=0; i<5;i++)
+        {
+            Thread t=new Thread(CR);
+            threads[i]=t;
+            threads[i].start();
+        }
+        for(int i=0; i<5;i++)
+        {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
