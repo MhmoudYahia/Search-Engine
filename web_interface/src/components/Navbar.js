62d75231ba857a5a1088ae6a05b385ea4734c0fb
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2023-04-23 06:50:40 +0200

    Web Interface done!

---
+++ web_interface/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import './Home.css'
+import { useNavigate } from "react-router-dom"
+import logo from './web-crawler64.png'
+import Searchbar from "./Searchbar";
+
+
+const Navbar = (props) => {
+  const navigate = useNavigate();
+  return ( 
+    <AppBar sx={{zIndex:'0'}}  position="fixed">
+      <Container sx={{width:'100%',padding:'5px'}} maxWidth="xl">
+        <Toolbar sx={{display:'flex',alignItems:'center',justifyContent:'center',gap:'12px',width:'100%'}} >
+          <div style={{ paddingLeft: '69px', cursor: 'pointer' }} onClick={() => navigate('/')}  className="logo">
+            <img src={logo} style={{width:'45px'}} alt="" />
+          </div>
+          <Searchbar target={props.target} />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
--- web_interface/src/components/ResultsPage.js
+++ web_interface/src/components/ResultsPage.js
@@ -1,15 +1,77 @@
+import { useParams } from "react-router-dom";
 import Result from "./result";
+import * as React from 'react';
 import './ResultsPage.css'
+import { ThemeContext } from "../App";
+import { useContext } from "react";
+import Navbar from "./Navbar";
+import { Link, MemoryRouter, Route, Routes, useLocation,useNavigate } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+
+
+
 const ResultsPage = (props) => {
-  const results=props.Results;
-  return (
-  <div className="Results_Container">
-    {
-    results.map((result,idx) => (
-    <Result key={idx} result={result}></Result>))
+  const theme=useContext(ThemeContext);
+  const location = useLocation();
+  const navigate=useNavigate();
+  const query = new URLSearchParams(location.search);
+  const page = parseInt(query.get('page') || '1', 10);
+  const {target}=useParams();
+  const results = [];
+  for (let i = 0; i < 27; i++) {
+    results[i] = {
+      url: "https://geeksforgeeks.org/",
+      title: "How to create a multithreaded search engine? "+(i+1),
+      siteName: "GeeksforGeeks",
+      description: "Lorem ipsum dolor sit amet mahmoud adipisicing elit. Eos enim sobhy saepe quisquam id nam vel totam mahmoud sobhy iure nemo vitae omnis eaque, numquam assumenda quasi molestias adipisci tempora placeat."
     }
-  </div> 
-   );
+  }
+  const myResults = results.slice((page - 1) * 12, (page * 12));
+  React.useEffect(() => {
+    const mode = theme;
+    document.documentElement.setAttribute("data-theme", mode);
+    Array.from(document.getElementsByTagName("p")).map((element) => element.setAttribute("data-theme", mode));
+    Array.from(document.getElementsByTagName("div")).map((element) => element.setAttribute("data-theme", mode));
+  }, [theme,page]);
+
+React.useEffect(() => {
+  document.querySelector(".Results_page").scrollIntoView({ behavior: 'smooth' });
+},[page]);
+
+  function Content() {
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const page = parseInt(query.get('page') || '1', 10);
+    return (
+      <Pagination
+        page={page}
+        sx={{paddingBottom:"15px",
+        margin:'auto'}}
+        count={Math.ceil(results.length / 12)}
+        renderItem={(item) => (
+          <PaginationItem
+            component={Link}
+            to={`./${item.page === 1 ? '' : `?page=${item.page}`}`}
+            {...item}
+          />
+        )}
+      />
+    );
+  }
+
+  return (
+    <div className="Results_page">
+    <Navbar target={target} />
+    <div className="Results_Container">
+      {
+      myResults.map((result,idx) => (
+      <Result key={idx} target={target} result={result}></Result >))
+      }
+    </div> 
+    <Content></Content>
+  </div>
+  );
 }
- 
+
 export default ResultsPage;
