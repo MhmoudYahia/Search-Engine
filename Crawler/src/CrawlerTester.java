0690abd43c7c8ecb44cf72aaabda8050f74766bc
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2023-05-06 00:11:01 +0300

    Robot.txt Safety Added!!

--- Crawler/src/CrawlerTester.java
+++ Crawler/src/CrawlerTester.java
@@ -1,15 +1,16 @@
 public class CrawlerTester {
+    final static int num_threads=6;
     public static void main(String[] args) {
 
         CrawlerIMP CR=new CrawlerIMP("./Seed/Seed.txt");
-        Thread[] threads=new Thread[5];
-        for(int i=0; i<5;i++)
+        Thread[] threads=new Thread[num_threads];
+        for(int i=0; i<num_threads;i++)
         {
             Thread t=new Thread(CR);
             threads[i]=t;
             threads[i].start();
         }
-        for(int i=0; i<5;i++)
+        for(int i=0; i<num_threads;i++)
         {
             try {
                 threads[i].join();
---
+++ Crawler/src/RobotParser.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.net.httpserver.HttpContext;
+import crawlercommons.robots.*;
+import org.apache.commons.io.IOUtils;
+
+public class RobotParser {
+
+    public static boolean isSafe(String url2)  {
+        String USER_AGENT = "*";
+        URL url= null;
+        try {
+            url = new URL(url2);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+        URL urlObj = url;
+        String hostId = urlObj.getProtocol() + "://" + urlObj.getHost()
+                + (urlObj.getPort() > -1 ? ":" + urlObj.getPort() : "");
+        String RobotFile="";
+        String str;
+        URL urll= null;
+        URLConnection urlCon= null;
+        try {
+            urll = new URL(hostId+"/robots.txt");
+            urlCon = urll.openConnection();
+            InputStreamReader in=new InputStreamReader((InputStream) urlCon.getContent());
+            BufferedReader bf=new BufferedReader(in);
+            while ((str=bf.readLine())!=null){
+                RobotFile+=str;
+                RobotFile+="\n";
+            }
+            bf.close();
+            in.close();
+        } catch (MalformedURLException e) {
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
+
+
+        SimpleRobotRulesParser RP=new SimpleRobotRulesParser();
+        BaseRobotRules rules= RP.parseContent(hostId,IOUtils.toByteArray(RobotFile),"text/plain",USER_AGENT);
+        boolean urlAllowed = rules.isAllowed(url.toString());
+        return urlAllowed;
+    }
+}
