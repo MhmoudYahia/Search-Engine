34397dbee0686ba6d7801d08e35e5d5e4d401bd9
Yousif Mohamed <yousif.infinix@gmail.com>
2023-05-15 22:10:43 +0300

    new regex

--- api/src/main/java/com/goat/searchengine/api/controller/QueryController.java
+++ api/src/main/java/com/goat/searchengine/api/controller/QueryController.java
@@ -3,16 +3,20 @@ package com.goat.searchengine.api.controller;
 import com.goat.searchengine.api.document.WordDocument;
 import com.goat.searchengine.api.model.Query;
 import com.goat.searchengine.api.repository.Repository;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.tartarus.snowball.ext.porterStemmer;
 
+import java.beans.PropertyEditorSupport;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.Math;
 import java.util.*;
+import org.tartarus.snowball.ext.porterStemmer;
 
 
 @RestController
@@ -24,6 +28,19 @@ public class QueryController {
 
     //max allowed gap between 2 words in phrase searching
     int phrase_gap = 10;
+
+
+//    private QueryService queryService;
+//
+//    @Autowired
+//    public QueryController(QueryService queryService){
+//        this.queryService=queryService;
+//    }
+//    @GetMapping("/")
+//    public List<Query> getResult(@RequestParam String q){
+//
+//        return queryService.getQueryResults(q);
+//    }
 
     public static String processStringWithStemming(String txt, String stopWords) {
 
@@ -68,7 +85,7 @@ public class QueryController {
             }
         } catch (FileNotFoundException e) {
             //Print an error message if an exception is thrown
-            System.out.println("An error occurred in loadStopWords.");
+            System.out.println("An error occurred.");
         }
         return stopWords;
     }
@@ -91,8 +108,8 @@ public class QueryController {
         }
         if(index != -1)
         {
-            int start = Math.max(index - 10, 0);
-            int end = Math.min(index + 20,size);
+            int start = Math.max(index - 15, 0);
+            int end = Math.min(index + 15,size);
             for(int i = start; i<end;i++)
             {
                 result.append(words[i]).append(" ");
@@ -115,6 +132,77 @@ public class QueryController {
         }
         return false;
     }
+
+    List<Query> AND_phrases(List<Query>res1, List<Query>res2)
+    {
+        List<Query> result = new ArrayList<>();
+        HashMap<String,Pair<Query,Integer>> map= new HashMap<>();
+        String url;
+        int count;
+        for(Query r1:res1)
+        {
+            url = r1.getUrl();
+            Pair<Query, Integer> pair = Pair.of(r1, 1);
+            map.put(url,pair);
+        }
+        for(Query r2:res2)
+        {
+            url = r2.getUrl();
+            count = map.containsKey(url)? map.get(url).getSecond() : 0;
+            Pair<Query, Integer> pair = Pair.of(r2,count + 1);
+            map.put(url,pair);
+        }
+        for(Map.Entry<String,Pair<Query,Integer>> entry: map.entrySet())
+        {
+            if(entry.getValue().getSecond() == 2)
+                result.add(entry.getValue().getFirst());
+        }
+        return result;
+    }List<Query> OR_phrases(List<Query>res1, List<Query>res2)
+    {
+        List<Query> result = new ArrayList<>();
+        HashMap<String,Query> map= new HashMap<>();
+        for(Query r1:res1)
+        {
+            map.put(r1.getUrl(),r1);
+        }
+        for(Query r2:res2)
+        {
+            map.put(r2.getUrl(),r2);
+        }
+        for(Map.Entry<String,Query> entry: map.entrySet())
+        {
+            result.add(entry.getValue());
+        }
+        return result;
+    }
+    List<Query> NOT_phrases(List<Query>res1, List<Query>res2)
+    {
+        List<Query> result = new ArrayList<>();
+        HashMap<String,Pair<Query,Integer>> map= new HashMap<>();
+        String url;
+        int count;
+        for(Query r1:res1)
+        {
+            url = r1.getUrl();
+            Pair<Query, Integer> pair = Pair.of(r1, 1);
+            map.put(url,pair);
+        }
+        for(Query r2:res2)
+        {
+            url = r2.getUrl();
+            count = map.containsKey(url)? map.get(url).getSecond() : 0;
+            Pair<Query, Integer> pair = Pair.of(r2,count - 1);
+            map.put(url,pair);
+        }
+        for(Map.Entry<String,Pair<Query,Integer>> entry: map.entrySet())
+        {
+            if(entry.getValue().getSecond() == 1)
+                result.add(entry.getValue().getFirst());
+        }
+        return result;
+    }
+
     public List<Query> phrase_search(String text)
     {
         String processed_text = processStringWithoutStemming(text,stop_words);
@@ -206,7 +294,6 @@ public class QueryController {
             Query q = new Query(url,title,desc,paragraph,score);
             results.add(q);
         }
-        Collections.sort(results);
         return results;
     }
 
@@ -300,7 +387,6 @@ public class QueryController {
             Query q = new Query(url,title,desc,paragraph,score);
             results.add(q);
         }
-        Collections.sort(results);
         return results;
     }
 
@@ -313,18 +399,47 @@ public List<Query> search(String text)
 
     //stem and then split the search text into words
     stop_words = loadStopWords();
-    List<Query> results;
+    List<List<Query>> results = new ArrayList<>();
+    int phrase_index = 1;   //the next phrase to be in an operation with the first phrase
+
     //phrase searching
     if(text.contains("\""))
     {
-        text = text.replaceAll("\"","");
-        results = phrase_search(text);
+        text = text.replaceFirst("\"","");
+        String[] phrases = text.split("\"");
+        for(String phrase:phrases)
+        {
+            if(!(phrase.equals(" AND ") || phrase.equals(" OR ") || phrase.equals(" NOT ")))
+            {
+                results.add(phrase_search(phrase));
+            }
+        }
+        for(String phrase:phrases)
+        {
+            if(phrase.equals(" AND "))
+            {
+                results.set(0, AND_phrases(results.get(0),results.get(phrase_index)));
+                phrase_index++;
+            }
+            else if(phrase.equals(" OR "))
+            {
+                results.set(0, OR_phrases(results.get(0),results.get(phrase_index)));
+                phrase_index++;
+            }
+            else if(phrase.equals(" NOT "))
+            {
+                results.set(0, NOT_phrases(results.get(0),results.get(phrase_index)));
+                phrase_index++;
+            }
+        }
     }
     //normal searching
     else
     {
-        results = normal_search(text);
+        results.add(normal_search(text));
     }
-    return results;
+    Collections.sort(results.get(0));
+    System.out.println(results.get(0).size());
+    return results.get(0);
 }
 }
